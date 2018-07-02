628894d3354bd30846c13fc27501fe91c3a6b41a
Kachel Wittig <kjwittig@gmail.com>
2018-07-02 17:39:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
 
       resp.write item.price
     else
-      resp.write "Route not found"  
+      resp.write "Route not found"
       resp.status = 404
     end
   resp.finish
